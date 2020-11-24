551d64eafc11b1eafd442f9a30a859383e939151
jb500 <janbckx11@gmail.com>
2020-11-24 11:04:54 +0100

    Added login component

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-git push -u origin mainimport { enableProdMode } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
